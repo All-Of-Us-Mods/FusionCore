b78511406cc02bce65049e9077226797137e63d1
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-03-23 23:01:22 -0400

    fix immersive styling

--- unityLibrary/src/main/java/com/unity3d/player/UnityPlayerActivity.java
+++ unityLibrary/src/main/java/com/unity3d/player/UnityPlayerActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import dev.allofus.fusioncore.ActivityBridge;
 import dev.allofus.fusioncore.CustomContextWrapper;
@@ -29,6 +33,36 @@ public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecyc
     protected String updateUnityCommandLineArguments(String cmdLine)
     {
         return cmdLine;
+    }
+
+    private void applyImmersiveMode()
+    {
+        Window window = getWindow();
+        if (window == null) {
+            return;
+        }
+
+        // Use decor view controller to avoid Window#getInsetsController() null-decor crash on startup.
+        View decorView = window.getDecorView();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = decorView.getWindowInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+                controller.hide(WindowInsets.Type.systemBars());
+            }
+        } else {
+            final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(flags);
+        }
     }
 
     // Setup activity layout
@@ -66,6 +100,7 @@ public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecyc
             //UnityPlayer.currentContext = wrappedContext;
             setContentView(mUnityPlayer);
             mUnityPlayer.requestFocus();
+            applyImmersiveMode();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -125,6 +160,7 @@ public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecyc
     @Override protected void onResume()
     {
         super.onResume();
+        applyImmersiveMode();
         mUnityPlayer.onResume();
     }
 
@@ -156,6 +192,9 @@ public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecyc
     @Override public void onWindowFocusChanged(boolean hasFocus)
     {
         super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyImmersiveMode();
+        }
         mUnityPlayer.windowFocusChanged(hasFocus);
     }
 
