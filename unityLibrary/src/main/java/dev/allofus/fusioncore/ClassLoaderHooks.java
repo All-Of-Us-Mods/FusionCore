dae0286d0e8197efcd7add95d0a136098a856f8b
IExploitableMan <pythonblack5000@gmail.com>
2026-03-27 00:34:21 +0300

    Dexloading schizo method

--- unityLibrary/src/main/java/dev/allofus/fusioncore/ClassLoaderHooks.java
+++ unityLibrary/src/main/java/dev/allofus/fusioncore/ClassLoaderHooks.java
@@ -1,6 +1,5 @@
 package dev.allofus.fusioncore;
 
-import android.content.pm.PackageManager;
 import android.util.Log;
 
 import java.lang.reflect.Method;
@@ -38,40 +37,6 @@ public class ClassLoaderHooks {
     }
 
     public static void installHooks(ClassLoader gameClassLoader) {
-
-        Method loadClassMethod = loadClassMethodViaReflection();
-
-        Pine.hook(loadClassMethod, new MethodHook() {
-            @Override
-            public void afterCall(Pine.CallFrame callFrame) {
-                try {
-                    // Only attempt fallback if class was not found
-                    if (callFrame.getThrowable() instanceof ClassNotFoundException) {
-                        String className = (String) callFrame.args[0];
-                        Log.d("ClassLoaderHooks", "afterLoadClass: class not found in default loader: " + className);
-
-                        // Skip fallback if this is already the game classloader
-                        if (callFrame.thisObject == gameClassLoader) {
-                            Log.d("ClassLoaderHooks", "Skipping fallback: loader is already game classloader");
-                            return;
-                        }
-
-                        // Attempt to load from game classloader, but don't let exceptions propagate to JNI
-                        try {
-                            Class<?> gameClass = gameClassLoader.loadClass(className);
-                            callFrame.setResult(gameClass);
-                            Log.d("ClassLoaderHooks", "Successfully loaded from game classloader: " + className);
-                        } catch (ClassNotFoundException e) {
-                            Log.d("ClassLoaderHooks", "Class not found in game classloader either: " + className);
-                        } catch (Exception e) {
-                            // Catch any other exceptions to prevent them from propagating to JNI
-                            Log.w("ClassLoaderHooks", "Unexpected error loading class from game classloader: " + className, e);
-                        }
-                    }
-                } catch (Exception e) {
-                    Log.e("ClassLoaderHooks", "Unexpected error in afterCall (CRITICAL)", e);
-                }
-            }
-        });
+        Log.i(TAG, "installHooks called, gameClassLoader: " + gameClassLoader);
     }
 }
