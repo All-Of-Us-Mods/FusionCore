5262c67ea89eff96ca0dd7350bd76cbdf0e1060c
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-08-26 23:14:30 -0400

    switch to 16kb compliant pine and update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("com.android.application") version "9.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
 
 repositories {
@@ -12,5 +12,6 @@ repositories {
 }
 
 tasks.register("clean", Delete::class) {
+    description = "clean build files"
     delete(rootProject.layout.buildDirectory)
 }
