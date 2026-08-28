992b753eba2891b8ed633b9412157976e600b40b
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-08-28 00:29:45 -0400

    im slow

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-unity.pro")
         }
     }
 
